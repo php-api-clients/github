70f502d977c1541af2a49b2f9e0f489e9fa41b46
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-06-09 07:58:32 +0200

    List user repositories sync

---
+++ examples/user-repositories.php
@@ -0,0 +1,16 @@
+<?php
+
+use WyriHaximus\Github\Client;
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+$client = new Client($loop);
+
+$repositories = $client->user($argv[1] ?? 'WyriHaximus')->repositories();
+
+foreach ($repositories as $repository) {
+    echo "\t", 'Repository', PHP_EOL;
+    echo "\t\t" . 'id: ' . $repository->id(), PHP_EOL;
+    echo "\t\t" . 'name: ' . $repository->name(), PHP_EOL;
+    echo "\t\t" . 'fork: ' . (int)$repository->fork(), PHP_EOL;
+}
