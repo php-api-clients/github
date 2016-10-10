1fcb8c16e39aa0cc231253a4036ffa9100f7b971
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-10-10 22:02:25 +0200

    IteratePagesCommand test

---
+++ tests/CommandBus/Command/IteratePagesCommandTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Github\CommandBus\Command;
+
+use ApiClients\Github\CommandBus\Command\IteratePagesCommand;
+use ApiClients\Tools\TestUtilities\TestCase;
+
+class IteratePagesCommandTest extends TestCase
+{
+    public function testCommand()
+    {
+        $path = '/foo.bar';
+        $command = new IteratePagesCommand($path);
+        $this->assertSame($path, $command->getPath());
+    }
+}
