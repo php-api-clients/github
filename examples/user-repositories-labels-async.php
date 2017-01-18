06c9a7a6c2570fd512577bac2bae17125847bdd4
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-01-18 21:57:03 +0100

    Show a repositories labels async

---
+++ examples/user-repositories-labels-async.php
@@ -0,0 +1,27 @@
+<?php
+
+use ApiClients\Client\Github\AsyncClient;
+use ApiClients\Client\Github\Resource\Async\Repository;
+use ApiClients\Client\Github\Resource\Async\User;
+use React\EventLoop\Factory;
+use function ApiClients\Foundation\resource_pretty_print;
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+$loop = Factory::create();
+
+$client = AsyncClient::create($loop, require 'resolve_token.php');
+
+$client->user($argv[1] ?? 'php-api-clients')->then(function (User $user) use ($argv) {
+    resource_pretty_print($user);
+    return $user->repository($argv[2] ?? 'github');
+})->done(function (Repository $repository) {
+    resource_pretty_print($repository, 1, true);
+    $repository->labels()->subscribeCallback(function ($labels) {
+        resource_pretty_print($labels, 2, true);
+    }, function ($error) {
+        echo (string)$error;
+    });
+});
+
+$loop->run();
