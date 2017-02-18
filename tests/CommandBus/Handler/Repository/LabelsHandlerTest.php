9202f1eaa0e2463a4c6abf2af831352bee7fdabb
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-02-18 18:27:24 +0100

    Branches and labels handler test

---
+++ tests/CommandBus/Handler/Repository/LabelsHandlerTest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Github\CommandBus\Handler\Repository;
+
+use ApiClients\Client\Github\CommandBus\Command\Repository\LabelsCommand;
+use ApiClients\Client\Github\CommandBus\Handler\Repository\LabelsHandler;
+use ApiClients\Client\Github\Resource\LabelInterface;
+use ApiClients\Client\Github\Service\IteratePagesService;
+use ApiClients\Foundation\Hydrator\Hydrator;
+use ApiClients\Tools\TestUtilities\TestCase;
+use Rx\Observable;
+use Rx\React\Promise;
+use function React\Promise\resolve;
+
+final class LabelsHandlerTest extends TestCase
+{
+    public function testCommand()
+    {
+        $labelArray = [
+            'foo' => 'bar',
+        ];
+        $label = $this->prophesize(LabelInterface::class)->reveal();
+
+        $command = new LabelsCommand('api-clients/github');
+
+        $iteratePagesService = $this->prophesize(IteratePagesService::class);
+        $iteratePagesService->handle('repos/api-clients/github/labels')->shouldBeCalled()->willReturn(resolve(Observable::fromArray([[$labelArray]])));
+
+        $hydrator = $this->prophesize(Hydrator::class);
+        $hydrator->hydrate(LabelInterface::HYDRATE_CLASS, $labelArray)->shouldBeCalled()->wilLReturn($label);
+
+        $labelsHandler = new LabelsHandler(
+            $iteratePagesService->reveal(),
+            $hydrator->reveal()
+        );
+
+        self::assertSame($label, $this->await(Promise::fromObservable($this->await($labelsHandler->handle($command)))));
+    }
+}
