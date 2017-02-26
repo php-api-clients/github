e7b552493bb59463d60d21f1bec1ef1c250c28b4
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-02-26 21:14:48 +0100

    Anonymous auth

---
+++ src/Authentication/Anonymous.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Client\Github\Authentication;
+
+use ApiClients\Client\Github\AuthenticationInterface;
+
+final class Anonymous implements AuthenticationInterface
+{
+    public function getOptions(): array
+    {
+        return [];
+    }
+}
