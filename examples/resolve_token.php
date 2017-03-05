fcd643c8d3a9975ce58c0b72f40e34fe1f778e62
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-05 18:57:00 +0100

    Support anonymous requests, but with a warning

--- examples/resolve_token.php
+++ examples/resolve_token.php
@@ -1,12 +1,15 @@
 <?php
 
+use ApiClients\Client\Github\Authentication\Anonymous;
 use ApiClients\Client\Github\Authentication\Token;
 
 $keyFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'token.php';
 
 if (!file_exists($keyFile)) {
     echo 'No key file find, copy token.sample.php to token.php and add a token from https://github.com/settings/tokens to run examples.', PHP_EOL;
-    exit(1);
+    echo 'We\'ll make Anonymous calls to Github, add a token to token.sample.php to authenticated requests.', PHP_EOL;
+    echo '-----------------------------------------------------------------------------------------------------------------------------', PHP_EOL;
+    return new Anonymous();
 }
 
 return new Token(require $keyFile);
