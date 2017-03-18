b6a97c9ad72157072a9e595c94c0aee1baa3c682
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-18 10:14:10 +0100

    Iterage pages changes

--- tests/CommandBus/Command/IteratePagesCommandTest.php
+++ tests/CommandBus/Command/IteratePagesCommandTest.php
@@ -5,12 +5,12 @@ namespace ApiClients\Tests\Github\CommandBus\Command;
 use ApiClients\Client\Github\CommandBus\Command\IteratePagesCommand;
 use ApiClients\Tools\TestUtilities\TestCase;
 
-class IteratePagesCommandTest extends TestCase
+final class IteratePagesCommandTest extends TestCase
 {
     public function testCommand()
     {
         $path = '/foo.bar';
         $command = new IteratePagesCommand($path);
-        $this->assertSame($path, $command->getPath());
+        self::assertSame($path, $command->getPath());
     }
 }
