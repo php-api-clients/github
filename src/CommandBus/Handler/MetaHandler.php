3aa3571011357dbf310c93e5dddc7acf3c88bd54
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-26 18:47:16 +0200

    Meta handler

---
+++ src/CommandBus/Handler/MetaHandler.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Client\Github\CommandBus\Handler;
+
+use ApiClients\Client\Github\CommandBus\Command\MetaCommand;
+use ApiClients\Client\Github\Resource\MetaInterface;
+use ApiClients\Tools\Services\Client\FetchAndHydrateService;
+use React\Promise\PromiseInterface;
+
+final class MetaHandler
+{
+    /**
+     * @var FetchAndHydrateService
+     */
+    private $service;
+
+    /**
+     * MetaHandler constructor.
+     * @param FetchAndHydrateService $service
+     */
+    public function __construct(FetchAndHydrateService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * @param MetaCommand $command
+     * @return PromiseInterface
+     */
+    public function handle(MetaCommand $command): PromiseInterface
+    {
+        return $this->service->fetch('meta', '', MetaInterface::HYDRATE_CLASS);
+    }
+}
---
+++ tests/CommandBus/Handler/MetaHandlerTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Github\CommandBus\Handler;
+
+use ApiClients\Client\Github\CommandBus\Command\MetaCommand;
+use ApiClients\Client\Github\CommandBus\Handler\MetaHandler;
+use ApiClients\Client\Github\Resource\MetaInterface;
+use ApiClients\Tools\Services\Client\FetchAndHydrateService;
+use ApiClients\Tools\TestUtilities\TestCase;
+use function React\Promise\resolve;
+
+final class MetaHandlerTest extends TestCase
+{
+    public function testCommand()
+    {
+        $meta = $this->prophesize(MetaInterface::class)->reveal();
+
+        $service = $this->prophesize(FetchAndHydrateService::class);
+        $service->fetch('meta', '', MetaInterface::HYDRATE_CLASS)->shouldBeCalled()->willReturn(resolve($meta));
+
+        $handler = new MetaHandler($service->reveal());
+
+        self::assertSame($meta, $this->await($handler->handle(new MetaCommand())));
+    }
+}
