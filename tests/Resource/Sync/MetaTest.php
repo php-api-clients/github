5fb98a648280c2ba4e6ed4743fccb4483bbb16df
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-26 18:38:22 +0200

    Meta resource

---
+++ tests/Resource/Sync/MetaTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Client\Github\Resource\Sync;
+
+use ApiClients\Tools\ResourceTestUtilities\AbstractResourceTest;
+use ApiClients\Client\Github\ApiSettings;
+use ApiClients\Client\Github\Resource\Meta;
+
+class MetaTest extends AbstractResourceTest
+{
+    public function getSyncAsync() : string
+    {
+        return 'Sync';
+    }
+    public function getClass() : string
+    {
+        return Meta::class;
+    }
+    public function getNamespace() : string
+    {
+        return Apisettings::NAMESPACE;
+    }
+}
