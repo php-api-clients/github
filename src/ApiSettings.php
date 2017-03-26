1301efd5a826acce3c8dc95dc544128664a7e3fc
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-26 16:19:20 +0200

    Add preview accept headers to ApiSettings for topics and license information on repository objects

--- src/ApiSettings.php
+++ src/ApiSettings.php
@@ -17,6 +17,17 @@ final class ApiSettings
 {
     const NAMESPACE = 'ApiClients\\Client\\Github\\Resource';
 
+    const ACCEPT_HEADER = [
+        // License on repository object: https://developer.github.com/v3/licenses/#licenses
+        'application/vnd.github.drax-preview+json',
+
+        // Topics on repository object: https://developer.github.com/v3/repos/#repositories
+        'application/vnd.github.mercy-preview+json',
+
+        // Default header: https://developer.github.com/v3/#current-version
+        'application/vnd.github.v3+json',
+    ];
+
     const TRANSPORT_OPTIONS = [
         FoundationOptions::HYDRATOR_OPTIONS => [
             HydratorOptions::NAMESPACE => self::NAMESPACE,
@@ -47,6 +58,10 @@ final class ApiSettings
         $options = options_merge(self::TRANSPORT_OPTIONS, $auth->getOptions());
         $options = options_merge($options, $suppliedOptions);
         $options[FoundationOptions::HYDRATOR_OPTIONS][HydratorOptions::NAMESPACE_SUFFIX] = $suffix;
+
+        $acceptHeader = implode('; ', self::ACCEPT_HEADER);
+        $options[FoundationOptions::TRANSPORT_OPTIONS][TransportOptions::HEADERS]['Accept'] = $acceptHeader;
+
         return $options;
     }
 }
