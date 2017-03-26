d7bc20fda7d0c9d4a1db8fe21cc9d08bf4845d77
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-26 14:16:22 +0200

    Added displayState function for examples

--- examples/resolve_token.php
+++ examples/resolve_token.php
@@ -2,6 +2,7 @@
 
 use ApiClients\Client\Github\Authentication\Anonymous;
 use ApiClients\Client\Github\Authentication\Token;
+use ApiClients\Client\Github\RateLimitState;
 
 $keyFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'token.php';
 
@@ -12,6 +13,13 @@ function display_throwable(Throwable $throwable)
     echo 'Message: ', $throwable->getMessage(), PHP_EOL;
     echo '-------------------------------', PHP_EOL;
     echo (string) $throwable, PHP_EOL;
+}
+
+function displayState(RateLimitState $state) {
+    echo 'Rate Limit State: ', PHP_EOL;
+    echo "\t", 'Limit: ', $state->getLimit(), PHP_EOL;
+    echo "\t", 'Remaining: ', $state->getRemaining(), PHP_EOL;
+    echo "\t", 'Reset: ', date(DATE_RFC2822, $state->getReset()), PHP_EOL;
 }
 
 //return new Anonymous();
