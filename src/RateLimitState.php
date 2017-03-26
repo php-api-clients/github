e38e99fffc06b0e8960fe900fa0bf4f5522734e3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-03-26 12:45:38 +0200

    Removed unused use statements

--- src/RateLimitState.php
+++ src/RateLimitState.php
@@ -2,18 +2,6 @@
 
 namespace ApiClients\Client\Github;
 
-use ApiClients\Client\Github\Middleware\RateLimitStatusMiddleware;
-use ApiClients\Foundation\Hydrator\Options as HydratorOptions;
-use ApiClients\Foundation\Options as FoundationOptions;
-use ApiClients\Foundation\Transport\Middleware\JsonDecodeMiddleware;
-use ApiClients\Foundation\Transport\Middleware\JsonEncodeMiddleware;
-use ApiClients\Foundation\Transport\Options as TransportOptions;
-use ApiClients\Middleware\HttpExceptions\HttpExceptionsMiddleware;
-use ApiClients\Middleware\UserAgent\Options as UserAgentMiddlewareOptions;
-use ApiClients\Middleware\UserAgent\UserAgentMiddleware;
-use ApiClients\Middleware\UserAgent\UserAgentStrategies;
-use function ApiClients\Foundation\options_merge;
-
 final class RateLimitState
 {
     /**
