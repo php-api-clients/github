2e8f0e19ba412c5a43793a052a7c6c0dcb51fafb
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-04-17 23:37:18 +0200

    Added test for IteratePagesHandler

---
+++ tests/CommandBus/Handler/IteratePagesHandlerTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Github\CommandBus\Handler;
+
+use ApiClients\Client\Github\CommandBus\Command\IteratePagesCommand;
+use ApiClients\Client\Github\CommandBus\Handler\IteratePagesHandler;
+use ApiClients\Client\Github\Service\IteratePagesService;
+use ApiClients\Tools\TestUtilities\TestCase;
+
+final class IteratePagesHandlerTest extends TestCase
+{
+    public function testCommand()
+    {
+        $path = 'path';
+
+        $service = $this->prophesize(IteratePagesService::class);
+        $service->iterate($path)->shouldBeCalled();
+
+        $handler = new IteratePagesHandler($service->reveal());
+        $handler->handle(new IteratePagesCommand($path));
+    }
+}
