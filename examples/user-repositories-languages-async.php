550fcc969121ffe8650534d5e59d1e8b9224c613
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-06-18 15:29:09 +0200

    Languages method on repository

---
+++ examples/user-repositories-languages-async.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+use ApiClients\Client\Github\AsyncClient;
+use ApiClients\Client\Github\Resource\Async\Repository;
+use ApiClients\Client\Github\Resource\Async\User;
+use React\EventLoop\Factory;
+use function ApiClients\Foundation\resource_pretty_print;
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+$loop = Factory::create();
+
+$client = AsyncClient::create($loop, require 'resolve_token.php');
+
+$client->user($argv[1] ?? 'php-api-clients')->then(function (User $user) use ($argv) {
+    resource_pretty_print($user);
+
+    return $user->repository($argv[2] ?? 'github');
+})->then(function (Repository $repository) {
+    resource_pretty_print($repository, 1, true);
+
+    return $repository->languages();
+})->done(function ($languages) {
+    var_export($languages);
+}, 'display_throwable');
+
+$loop->run();
+
+displayState($client->getRateLimitState());
---
+++ tests/Resource/Sync/Repository/Release/EmptyAssetTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Client\Github\Resource\Sync\Repository\Release;
+
+use ApiClients\Client\Github\Resource\Sync\Repository\Release\EmptyAsset;
+use ApiClients\Tools\ResourceTestUtilities\AbstractEmptyResourceTest;
+
+final class EmptyAssetTest extends AbstractEmptyResourceTest
+{
+    public function getSyncAsync(): string
+    {
+        return 'Sync';
+    }
+
+    public function getClass(): string
+    {
+        return EmptyAsset::class;
+    }
+}
