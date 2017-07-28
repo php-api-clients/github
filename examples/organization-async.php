46b3ccc8c713ed4179e6b3199ccc6cd8131a1a82
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-07-28 23:14:14 +0200

    Get org from client

---
+++ examples/organization-async.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+use ApiClients\Client\Github\AsyncClient;
+use ApiClients\Client\Github\Resource\Async\Organization;
+use React\EventLoop\Factory;
+use function ApiClients\Foundation\resource_pretty_print;
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+$loop = Factory::create();
+$client = AsyncClient::create($loop, require 'resolve_token.php');
+
+$organizations = [
+    'php-api-clients',
+    'reactphp',
+    'recoilphp',
+];
+
+if (count($argv) > 1) {
+    unset($argv[0]);
+    foreach ($argv as $organization) {
+        $organizations[] = $organization;
+    }
+}
+
+foreach ($organizations as $organization) {
+    $client->organization($organization)->then(function (Organization $user) {
+        resource_pretty_print($user);
+
+        return $user->refresh();
+    })->done(function (Organization $user) {
+        resource_pretty_print($user);
+    }, 'display_throwable');
+}
+
+$loop->run();
+
+displayState($client->getRateLimitState());
---
+++ tests/CommandBus/Handler/OrganizationHandlerTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Github\CommandBus\Handler;
+
+use ApiClients\Client\Github\CommandBus\Command\OrganizationCommand;
+use ApiClients\Client\Github\CommandBus\Handler\OrganizationHandler;
+use ApiClients\Client\Github\Resource\OrganizationInterface;
+use ApiClients\Tools\Services\Client\FetchAndHydrateService;
+use ApiClients\Tools\TestUtilities\TestCase;
+
+final class OrganizationHandlerTest extends TestCase
+{
+    public function testCommand()
+    {
+        $service = $this->prophesize(FetchAndHydrateService::class);
+        $service->fetch('orgs/php-api-clients', '', OrganizationInterface::HYDRATE_CLASS)->shouldBeCalled();
+
+        $handler = new OrganizationHandler($service->reveal());
+
+        $handler->handle(new OrganizationCommand('php-api-clients'));
+    }
+}
