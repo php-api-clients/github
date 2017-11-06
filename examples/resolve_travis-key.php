b395614bae5326bf4916f3de6198cc210e6e3c01
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-11-06 21:58:47 +0100

    cs-fix

--- examples/resolve_travis-key.php
+++ examples/resolve_travis-key.php
@@ -3,7 +3,9 @@
 $keyFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'travis-key.php';
 
 if (!file_exists($keyFile)) {
-    echo 'No key file find, copy key.sample.php to key.php and add access token from https://blog.travis-ci.com/2013-01-28-token-token-token/ to run examples.', PHP_EOL;
+    echo 'No key file find, copy key.sample.php to key.php and add access ',
+        'token from https://blog.travis-ci.com/2013-01-28-token-token-token/',
+        ' to run examples.', PHP_EOL;
     exit(1);
 }
 
