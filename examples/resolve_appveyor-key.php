f76db9d8b4fbb47ff2107ca4acbdda6ffbc5e3d1
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-12-07 22:06:15 +0100

    Fixed key resolver line length

--- examples/resolve_appveyor-key.php
+++ examples/resolve_appveyor-key.php
@@ -3,7 +3,8 @@
 $keyFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'appveyor-key.php';
 
 if (!file_exists($keyFile)) {
-    echo 'No key file found, copy key.sample.php to key.php and add API token from https://ci.appveyor.com/api-token to run examples.', PHP_EOL;
+    echo 'No key file found, copy key.sample.php to key.php and add API token from' .
+         ' https://ci.appveyor.com/api-token to run examples.', PHP_EOL;
     exit(1);
 }
 
