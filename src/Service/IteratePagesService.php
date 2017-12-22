3c38c89f859d7ddaf0cb1cb1cd0918ef41a6f841
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-12-22 17:39:14 +0100

    Use the new getParsedContents over getJson for parsed bodies

--- src/Service/IteratePagesService.php
+++ src/Service/IteratePagesService.php
@@ -66,7 +66,7 @@ class IteratePagesService
                 });
             })
             ->map(function (ResponseInterface $response) {
-                return $response->getBody()->getJson();
+                return $response->getBody()->getParsedContents();
             });
     }
 
@@ -89,7 +89,7 @@ class IteratePagesService
         ResponseInterface $response,
         Subject $subject
     ) {
-        $subject->onNext($response->getBody()->getJson());
+        $subject->onNext($response->getBody()->getParsedContents());
 
         if ($subject->isDisposed() || !$subject->hasObservers()) {
             $subject->onCompleted();
