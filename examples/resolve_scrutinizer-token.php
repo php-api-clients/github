6d5750fdf985a2531ea6511efb5b339c307a3997
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-12-23 23:24:17 +0100

    Fixed line length

--- examples/resolve_scrutinizer-token.php
+++ examples/resolve_scrutinizer-token.php
@@ -3,7 +3,8 @@
 $keyFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'scrutinizer-token.php';
 
 if (!file_exists($keyFile)) {
-    echo 'No key file find, copy token.sample.php to token.php and add a token from https://scrutinizer-ci.com/profile/applications to run examples.', PHP_EOL;
+    echo 'No key file find, copy token.sample.php to token.php and add a token ',
+         'from https://scrutinizer-ci.com/profile/applications to run examples.', PHP_EOL;
     exit(1);
 }
 
