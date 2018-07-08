a80bec9bb252df9cefb106e87e1308218a7a2979
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-07-08 15:25:12 +0200

    Error middleware that turns HTTP Exceptions in useful exceptions with errors

--- src/ApiSettings.php
+++ src/ApiSettings.php
@@ -2,6 +2,7 @@
 
 namespace ApiClients\Client\Github;
 
+use ApiClients\Client\Github\Middleware\ErrorMiddleware;
 use ApiClients\Client\Github\Middleware\RateLimitStateMiddleware;
 use ApiClients\Foundation\Hydrator\Options as HydratorOptions;
 use ApiClients\Foundation\Options as FoundationOptions;
@@ -28,9 +29,10 @@ final class ApiSettings
             TransportOptions::MIDDLEWARE => [
                 JsonDecodeMiddleware::class,
                 JsonEncodeMiddleware::class,
-                HttpExceptionsMiddleware::class,
                 UserAgentMiddleware::class,
                 RateLimitStateMiddleware::class,
+                HttpExceptionsMiddleware::class,
+                ErrorMiddleware::class,
             ],
             TransportOptions::DEFAULT_REQUEST_OPTIONS => [
                 UserAgentMiddleware::class => [
