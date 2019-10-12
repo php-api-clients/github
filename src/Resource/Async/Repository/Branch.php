3adba20eab9f647ed2f0a11af817e43c71f38664
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-10-12 17:49:07 +0200

    Get detailed commit from branch

--- src/Resource/Async/Repository/Branch.php
+++ src/Resource/Async/Repository/Branch.php
@@ -2,12 +2,22 @@
 
 namespace ApiClients\Client\Github\Resource\Async\Repository;
 
+use ApiClients\Client\Github\CommandBus\Command\Repository\DetailedCommitCommand;
 use ApiClients\Client\Github\Resource\Repository\Branch as BaseBranch;
+use React\Promise\PromiseInterface;
 
 class Branch extends BaseBranch
 {
     public function refresh(): Branch
     {
         throw new \Exception('TODO: create refresh method!');
+    }
+
+    public function detailedCommit(): PromiseInterface
+    {
+        return $this->handleCommand(new DetailedCommitCommand(
+            \str_replace('/branches/' . $this->name . '/protection', '', \explode('/repos/', $this->protection_url)[1]),
+            $this->commit->sha()
+        ));
     }
 }
