88a273d8a8ad101e8bde8cc1a7f25ec11077fc7b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-10-17 16:08:00 +0200

    Compare commits

---
+++ tests/Resource/Async/Repository/EmptyCompareTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Client\Github\Resource\Async\Repository;
+
+use ApiClients\Client\Github\Resource\Async\Repository\EmptyCompare;
+use ApiClients\Tools\ResourceTestUtilities\AbstractEmptyResourceTest;
+
+final class EmptyCompareTest extends AbstractEmptyResourceTest
+{
+    public function getSyncAsync(): string
+    {
+        return 'Async';
+    }
+
+    public function getClass(): string
+    {
+        return EmptyCompare::class;
+    }
+}
