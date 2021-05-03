1c5439f2dc40e5a636a5a9217e84a99532213fd5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-05-03 17:01:50 +0200

    wafefaeafew

---
+++ src/OpenAPI/GitHubEnterprise/v2_20/Path/.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ApiClients\Client\Github\OpenAPI\GitHubEnterprise\v2_20\.;
+
+final class 
+{
+    function get() : \ApiClients\Client\Github\OpenAPI\GitHubEnterprise\v2_20\Operation\Meta\RootOperation
+    {
+        return new \ApiClients\Client\Github\OpenAPI\GitHubEnterprise\v2_20\Operation\Meta\RootOperation();
+    }
+}
---
+++ src/OpenAPI/GitHubEnterprise/v2_22/Path/.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ApiClients\Client\Github\OpenAPI\GitHubEnterprise\v2_22\.;
+
+final class 
+{
+    function get() : \ApiClients\Client\Github\OpenAPI\GitHubEnterprise\v2_22\Operation\Meta\RootOperation
+    {
+        return new \ApiClients\Client\Github\OpenAPI\GitHubEnterprise\v2_22\Operation\Meta\RootOperation();
+    }
+}
