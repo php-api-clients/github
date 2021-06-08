fba8b3ea8e01bbf90c35565f23e03868a5858b22
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-06-08 20:55:41 +0200

    Keeping it slim for now

--- src/OpenAPI/GitHubAE/Path/Repos/CbOwnerRcb/CbRepoRcb/Releases/CbReleaseIdRcb/Reactions/CbReactionIdRcb.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace ApiClients\Client\Github\OpenAPI\GitHubAE\Path\Repos\CbOwnerRcb\CbRepoRcb\Releases\CbReleaseIdRcb\Reactions;
-
-final class CbReactionIdRcb
-{
-}
