e5d61fadb621f93e34190b175fb1e02b50f92d7b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-07-18 17:14:43 +0200

    Node code

---
+++ src/OpenAPI/ApiGitHubCom/Schema/TimelineAssignedIssueEvent.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema;
+
+final class TimelineAssignedIssueEvent
+{
+    public const SCHEMA_TITLE = 'Timeline Assigned Issue Event';
+    public const SCHEMA_DESCRIPTION = 'Timeline Assigned Issue Event';
+    private int $id;
+    private string $node_id;
+    private string $url;
+    /**
+     * Simple User
+     * @\WyriHaximus\Hydrator\Attribute\Hydrate(\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\SimpleUser::class)
+     */
+    private ?\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\SimpleUser $actor = null;
+    private string $event;
+    private ?string $commit_id = null;
+    private ?string $commit_url = null;
+    private string $created_at;
+    /**
+     * GitHub apps are a new way to extend GitHub. They can be installed directly on organizations and user accounts and granted access to specific repositories. They come with granular permissions and built-in webhooks. GitHub apps are first class actors within GitHub.
+     * @\WyriHaximus\Hydrator\Attribute\Hydrate(\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\Integration::class)
+     */
+    private ?\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\Integration $performed_via_github_app = null;
+    /**
+     * Simple User
+     * @\WyriHaximus\Hydrator\Attribute\Hydrate(\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\SimpleUser::class)
+     */
+    private ?\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\SimpleUser $assignee = null;
+    public function id() : int
+    {
+        return $this->id;
+    }
+    public function node_id() : string
+    {
+        return $this->node_id;
+    }
+    public function url() : string
+    {
+        return $this->url;
+    }
+    /**
+     * Simple User
+     */
+    public function actor() : ?\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\SimpleUser
+    {
+        return $this->actor;
+    }
+    public function event() : string
+    {
+        return $this->event;
+    }
+    public function commit_id() : ?string
+    {
+        return $this->commit_id;
+    }
+    public function commit_url() : ?string
+    {
+        return $this->commit_url;
+    }
+    public function created_at() : string
+    {
+        return $this->created_at;
+    }
+    /**
+     * GitHub apps are a new way to extend GitHub. They can be installed directly on organizations and user accounts and granted access to specific repositories. They come with granular permissions and built-in webhooks. GitHub apps are first class actors within GitHub.
+     */
+    public function performed_via_github_app() : ?\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\Integration
+    {
+        return $this->performed_via_github_app;
+    }
+    /**
+     * Simple User
+     */
+    public function assignee() : ?\ApiClients\Client\Github\OpenAPI\ApiGitHubCom\Schema\SimpleUser
+    {
+        return $this->assignee;
+    }
+}
