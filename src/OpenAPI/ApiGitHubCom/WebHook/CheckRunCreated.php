588c6d28f9019c1a21890c8559cfe320b7609a46
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-01-17 09:51:16 +0100

    Fix webhook key issues

---
+++ src/OpenAPI/ApiGitHubCom/WebHook/CheckRunCreated.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ApiClients\Client\Github\OpenAPI\ApiGitHubCom\WebHook;
+
+final class CheckRunCreated implements \ApiClients\Client\Github\OpenAPI\ApiGitHubCom\WebHookInterface
+{
+    public function resolve(array $data) : string
+    {
+        return 'ApiClients\\Client\\Github\\OpenAPI\\ApiGitHubCom\\Schema\\WebhookCheckRunCreated';
+    }
+}
