a2f0fccc0065b0ecd253bf649b80986388d428ec
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-03-06 22:57:04 +0100

    Updates

--- src/Schema/CommitActivity.php
+++ src/Schema/CommitActivity.php
@@ -12,7 +12,7 @@ final readonly class CommitActivity
     public const SCHEMA_JSON = '{"title":"Commit Activity","required":["days","total","week"],"type":"object","properties":{"days":{"type":"array","items":{"type":"integer"},"examples":[0,3,26,20,39,1,0]},"total":{"type":"integer","examples":[89]},"week":{"type":"integer","examples":[1336280400]}},"description":"Commit Activity"}';
     public const SCHEMA_TITLE = 'Commit Activity';
     public const SCHEMA_DESCRIPTION = 'Commit Activity';
-    public const SCHEMA_EXAMPLE_DATA = '{"days":[0],"total":89,"week":1336280400}';
+    public const SCHEMA_EXAMPLE_DATA = '{"days":[20],"total":89,"week":1336280400}';
     /**
      * @param ?array<int> $days
      */
--- src/Schema/PagesHttpsCertificate.php
+++ src/Schema/PagesHttpsCertificate.php
@@ -12,7 +12,7 @@ final readonly class PagesHttpsCertificate
     public const SCHEMA_JSON = '{"title":"Pages Https Certificate","required":["state","description","domains"],"type":"object","properties":{"state":{"enum":["new","authorization_created","authorization_pending","authorized","authorization_revoked","issued","uploaded","approved","errored","bad_authz","destroy_pending","dns_changed"],"type":"string","examples":["approved"]},"description":{"type":"string","examples":["Certificate is approved"]},"domains":{"type":"array","items":{"type":"string"},"description":"Array of the domain set and its alternate name (if it is configured)","examples":["example.com","www.example.com"]},"expires_at":{"type":"string","format":"date"}}}';
     public const SCHEMA_TITLE = 'Pages Https Certificate';
     public const SCHEMA_DESCRIPTION = '';
-    public const SCHEMA_EXAMPLE_DATA = '{"state":"approved","description":"Certificate is approved","domains":["example.com"],"expires_at":"generated_expires_at"}';
+    public const SCHEMA_EXAMPLE_DATA = '{"state":"approved","description":"Certificate is approved","domains":["www.example.com"],"expires_at":"generated_expires_at"}';
     /**
      * domains: Array of the domain set and its alternate name (if it is configured)
      * @param ?array<string> $domains
